ca292fa65c5c5d3e5cdf53e34997f09471ba20a4
Saifurrahmanemon <saifemon789@gmail.com>
2022-05-08 12:31:52 +0600

    finish with jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
@@ -8,6 +9,26 @@ const app = express();
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+//  jwt verification
+function verifyJWT(req, res, next) {
+    const getAuthHeader = req.headers.authorization;
+    if (!getAuthHeader) {
+        return res.status(401).send({ message: "unauthorize access" });
+    }
+
+    const getToken = getAuthHeader.split(" ")[1];
+    jwt.verify(getToken, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res
+                .status(403)
+                .send("forbidden access!!😕 forbidden access");
+        }
+        console.log("decoded", decoded);
+        res.decoded = decoded;
+        next();
+    });
+}
 
 app.get("/", (req, res) => {
     res.send("server is running");
@@ -28,6 +49,15 @@ const run = async () => {
         const inventoriesCollection = client
             .db("Riders-warehouse")
             .collection("inventories");
+
+        //FOR JWT AUTH
+        app.post("/login", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: "1d",
+            });
+            res.send({ accessToken });
+        });
 
         // get all inventories
         app.get("/inventories", async (req, res) => {
@@ -57,6 +87,41 @@ const run = async () => {
             const query = { _id: ObjectId(id) };
             const result = await inventoriesCollection.deleteOne(query);
             res.send(result);
+        });
+        // UPDATE QUANTITY
+        app.put("/inventories/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $inc: {
+                    quantity: updatedQuantity.updatedQuantityValue,
+                },
+            };
+            const result = await inventoriesCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            res.send(result);
+        });
+
+        //SEARCH BY EMAIL
+        app.get("/myItems", verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            console.log(req.decoded);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = inventoriesCollection.find(query);
+                const myItems = await cursor.toArray();
+                res.send(myItems);
+            } else {
+                return res
+                    .status(403)
+                    .send("forbidden access!!😕 forbidden access");
+            }
         });
 
         console.log("mongodb connected");
