9cd66a3bcc6fbaeaefbb8397bfa6526482c33279
Saifurrahmanemon <saifemon789@gmail.com>
2022-05-27 10:14:05 +0600

    deploy

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ app.use(express.json());
 
 //  jwt verification
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: "unauthorized access" });
-    }
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-        if (err) {
-            return res
-                .status(403)
-                .send({ message: "can not go further!!😕 forbidden access" });
-        }
+   const authHeader = req.headers.authorization;
+   if (!authHeader) {
+      return res.status(401).send({ message: "unauthorized access" });
+   }
+   const token = authHeader.split(" ")[1];
+   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+      if (err) {
+         return res
+            .status(403)
+            .send({ message: "can not go further!!😕 forbidden access" });
+      }
 
-        req.decoded = decoded;
-        next();
-    });
+      req.decoded = decoded;
+      next();
+   });
 }
 
 //DATABASE
@@ -34,105 +34,105 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${username}:${password}@cluster0.rhqv8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
 });
 
 const run = async () => {
-    try {
-        await client.connect();
-        const inventoriesCollection = client
-            .db("Riders-warehouse")
-            .collection("inventories");
+   try {
+      await client.connect();
+      const inventoriesCollection = client
+         .db("testRiders")
+         .collection("inventories");
 
-        //FOR JWT AUTH
-        app.post("/login", async (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
-                expiresIn: "1d",
-            });
-            res.send({ accessToken });
-        });
+      //FOR JWT AUTH
+      app.post("/login", async (req, res) => {
+         const user = req.body;
+         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+            expiresIn: "1d",
+         });
+         res.send({ accessToken });
+      });
 
-        // get all inventories
-        app.get("/inventories", async (req, res) => {
-            const query = {};
+      // get all inventories
+      app.get("/inventories", async (req, res) => {
+         const query = {};
+         const cursor = inventoriesCollection.find(query);
+         const inventories = await cursor.toArray();
+         res.send(inventories);
+      });
+      // for single inventory
+      app.get("/inventories/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const inventory = await inventoriesCollection.findOne(query);
+
+         res.send(inventory);
+      });
+      //ADD NEW INVENTORY
+      app.post("/addInventory", async (req, res) => {
+         const newInventory = req.body;
+
+         const result = await inventoriesCollection.insertOne(newInventory);
+         res.send(result);
+      });
+      //DELETE A INVENTORY
+      app.delete("/inventories/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const result = await inventoriesCollection.deleteOne(query);
+         res.send(result);
+      });
+      // UPDATE QUANTITY
+      app.put("/inventories/:id", async (req, res) => {
+         const id = req.params.id;
+         const updatedQuantity = req.body;
+         const filter = { _id: ObjectId(id) };
+         const options = { upsert: true };
+         const updateDoc = {
+            $inc: {
+               quantity: updatedQuantity.updatedQuantityValue,
+            },
+         };
+         const result = await inventoriesCollection.updateOne(
+            filter,
+            updateDoc,
+            options
+         );
+         res.send(result);
+      });
+
+      //SEARCH BY EMAIL
+      app.get("/myItems", verifyJWT, async (req, res) => {
+         const email = req.query.email;
+         const decodedEmail = req.decoded.email;
+
+         if (email === decodedEmail) {
+            const query = { email: email };
             const cursor = inventoriesCollection.find(query);
-            const inventories = await cursor.toArray();
-            res.send(inventories);
-        });
-        // for single inventory
-        app.get("/inventories/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const inventory = await inventoriesCollection.findOne(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+         } else {
+            res.status(403).send({
+               message: "can not go further!!😕 forbidden access",
+            });
+         }
+      });
 
-            res.send(inventory);
-        });
-        //ADD NEW INVENTORY
-        app.post("/addInventory", async (req, res) => {
-            const newInventory = req.body;
-
-            const result = await inventoriesCollection.insertOne(newInventory);
-            res.send(result);
-        });
-        //DELETE A INVENTORY
-        app.delete("/inventories/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await inventoriesCollection.deleteOne(query);
-            res.send(result);
-        });
-        // UPDATE QUANTITY
-        app.put("/inventories/:id", async (req, res) => {
-            const id = req.params.id;
-            const updatedQuantity = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updateDoc = {
-                $inc: {
-                    quantity: updatedQuantity.updatedQuantityValue,
-                },
-            };
-            const result = await inventoriesCollection.updateOne(
-                filter,
-                updateDoc,
-                options
-            );
-            res.send(result);
-        });
-
-        //SEARCH BY EMAIL
-        app.get("/myItems", verifyJWT, async (req, res) => {
-            const email = req.query.email;
-            const decodedEmail = req.decoded.email;
-
-            if (email === decodedEmail) {
-                const query = { email: email };
-                const cursor = inventoriesCollection.find(query);
-                const orders = await cursor.toArray();
-                res.send(orders);
-            } else {
-                res.status(403).send({
-                    message: "can not go further!!😕 forbidden access",
-                });
-            }
-        });
-
-        console.log("mongodb connected");
-    } catch (err) {
-        console.log(err);
-    } finally {
-    }
+      console.log("mongodb connected");
+   } catch (err) {
+      console.log(err);
+   } finally {
+   }
 };
 
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("server is running");
+   res.send("server is running");
 });
 //listen
 app.listen(port, (req, res) => {
-    console.log("server is running at port", port);
+   console.log("server is running at port", port);
 });
